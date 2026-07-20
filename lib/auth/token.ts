62e1c32b78993c78029df1494d04b74566ae3663
Jordan Thirkle <jordantthirkle@gmail.com>
2026-07-20 20:11:32 +0100

    chore: save progress

---
+++ lib/auth/token.ts
@@ -0,0 +1,23 @@
+import { SignJWT, jwtVerify } from 'jose';
+
+const key = new TextEncoder().encode(process.env.AUTH_SECRET);
+
+type SessionData = {
+  user: { id: number };
+  expires: string;
+};
+
+export async function signToken(payload: SessionData) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('1 day from now')
+    .sign(key);
+}
+
+export async function verifyToken(input: string) {
+  const { payload } = await jwtVerify(input, key, {
+    algorithms: ['HS256'],
+  });
+  return payload as SessionData;
+}
